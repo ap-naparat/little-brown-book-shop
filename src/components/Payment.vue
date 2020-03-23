97470f6dd9ad5c37699432b20902d9c45a0e82f8
jommiiz <ap.naparat@gmail.com>
2020-03-23 13:06:14 +0700

    add payment page

---
+++ src/components/Payment.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <div class="payment__content" v-if="cart.length !== 0">
+      <div>Payment Method : Cash</div>
+      <div class="payment__total">
+        <label>Total : </label>
+        <span>{{ totalNet }}</span>
+      </div>
+      <div>
+        <label>Cash : </label>
+        <input class="payment__amount" type="text" v-model="paidAmount" />
+      </div>
+      <div>
+          <label>Change : {{ cashBack }} ฿ </label>
+      </div>
+       <button to="/receipt" class="button__middle" @click="placeOrder()" :disabled="!validatePay">Place Order</button>
+    </div>
+    <div v-if="cart.length === 0">
+      <h3>Please choose book to your cart before payment</h3>
+      <router-link to="/">Go Shopping Now</router-link>
+    </div>
+  </div>
+</template>
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'Payment',
+  data () {
+    return {
+      paidAmount: ''
+    }
+  },
+  computed: {
+    ...mapGetters({
+      totalNet: 'totalNet',
+      cart: 'cartList'
+    }),
+    cashBack () {
+      const paidAmountNumber = parseInt(this.paidAmount, 10)
+      if (paidAmountNumber > this.totalNet) {
+        return parseInt(this.paidAmount, 10) - parseInt(this.totalNet, 10)
+      }
+      return 0
+    },
+    validatePay () {
+      return (
+        this.paidAmount > 0 && this.cashBack >= 0 && this.paidAmount >= this.totalNet
+      )
+    }
+  },
+  methods: {
+    placeOrder () {
+      const self = this
+      self.$router.push('/receipt')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.payment {
+    width: 80%;
+    font-size:20px;
+    padding:10px;
+    margin:0 auto;
+    text-align: left;
+    &__content {
+      border: 1px solid lightgrey;
+      padding: 30px;
+    }
+    &__amount {
+        font-size: 20px;
+        border-radius: 10px;
+        padding:10px 10px;
+        border:1px solid grey;
+        width: 50%;
+        &:focus {
+            outline: 0 none;
+        }
+    }
+}
+</style>
---
+++ src/views/Payment.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="payment">
+    <CartList />
+    <Payment />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Payment from '@/components/Payment.vue'
+import CartList from '@/components/CartList.vue'
+
+export default {
+  name: 'PAYMENT',
+  components: {
+    Payment,
+    CartList
+  }
+}
+</script>
+<style lang="scss">
+  .payment {
+    margin-top: 80px;
+    h1 {
+      font-size: 4em;
+      color: #07b53b;
+    }
+  }
+</style>
